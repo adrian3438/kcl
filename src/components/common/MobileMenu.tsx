2cfafcb77d9e63afdb1c998a79729b1720599481
jdlee <jdlee@bmcnet.kr>
2024-11-26 18:28:38 +0900

    Header 진행중 Footer 퍼블리싱 완료 관리자, 언서 세팅 완료

---
+++ src/components/common/MobileMenu.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+interface Props {
+    language: any
+}
+
+export default function MobileMenu({language}: Props) {
+    return (
+        <nav>
+            <ul>
+                <li><Link href="#">{language?.header_1}</Link></li>
+                <li><Link href="#">{language?.header_2}</Link></li>
+                <li><Link href="#">{language?.header_3}</Link></li>
+                <li><Link href="#">{language?.header_4}</Link></li>
+                <li><Link href="#">{language?.header_5}</Link></li>
+            </ul>
+            <ul>
+                <li>
+                    <ul>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                    </ul>
+                </li>
+                <li>&nbsp;</li>
+                <li>
+                    <ul>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                        <li><Link href="#"></Link></li>
+                    </ul>
+                </li>
+                <li>&nbsp;</li>
+            </ul>
+        </nav>
+    )
+}
