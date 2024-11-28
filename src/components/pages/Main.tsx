d8db3441396c68aee7d37f9e4942d4a4226810d4
jdlee <jdlee@bmcnet.kr>
2024-11-28 13:31:52 +0900

    메인 퍼블리싱 완료

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,5 +1,7 @@
 import '@/assets/main.scss';
 import Section01 from "@/components/main/Section01";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
     language: any
@@ -11,17 +13,134 @@ export default function Main({language}: Props) {
             <Section01 language={language}/>
             <section className="section-02">
                 <div>
-
+                    <div>
+                        <video
+                            autoPlay
+                            muted
+                            loop
+                            playsInline
+                            className="main-video-01"
+                        >
+                            <source src="/video/kclvalve-main-video-1.mp4" type="video/mp4"/>
+                            Your browser does not support the video tag.
+                        </video>
+                        <Link href="#">
+                            <p>OOO Test</p>
+                            <p>Go to OOO valve</p>
+                        </Link>
+                    </div>
+                    <div>
+                        <video
+                            autoPlay
+                            muted
+                            loop
+                            playsInline
+                            className="main-video-02"
+                        >
+                            <source src="/video/kclvalve-main-video-2.mp4" type="video/mp4"/>
+                            Your browser does not support the video tag.
+                        </video>
+                        <Link href="#">
+                            <p>OOO Test</p>
+                            <p>Go to OOO valve</p>
+                        </Link>
+                    </div>
                 </div>
                 <div>
-
+                    <div>
+                        <video
+                            autoPlay
+                            muted
+                            loop
+                            playsInline
+                            className="main-video-03"
+                        >
+                            <source src="/video/kclvalve-main-video-3.mp4" type="video/mp4"/>
+                            Your browser does not support the video tag.
+                        </video>
+                        <Link href="#">
+                            <p>OOO Test</p>
+                            <p>Go to OOO valve</p>
+                        </Link>
+                    </div>
                 </div>
             </section>
-            <section className="section-03"></section>
-            <section className="section-04"></section>
-            <section className="section-05"></section>
-            <section className="section-06"></section>
-            <section className="section-087"></section>
+            <section className="section-03 main-section">
+                <Image src="/images/main/section-03-bg.png" alt="" width={1919} height={1036}/>
+                <div className="section-text">
+                    <div>
+                        <div>
+                            <p className="section-title">{language?.main_01_22}</p>
+                            <p className="section-text-01">{language?.main_01_23}</p>
+                            <p className="section-text-02">{language?.main_01_24}</p>
+                            <div>
+                                <Link href="#" className="btn-01">{language?.main_01_25}</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-04 main-section">
+                <Image src="/images/main/section-04-bg.png" alt="" width={1920} height={1076}/>
+                <div className="section-text">
+                    <div>
+                        <div>
+                            <p className="section-title">{language?.main_01_26}</p>
+                            <p className="section-text-01">{language?.main_01_27}</p>
+                            <div>
+                                <Link href="#" className="btn-01">{language?.main_01_28}</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-05 main-section">
+                <Image src="/images/main/section-05-bg.png" alt="" width={1920} height={1088}/>
+                <div className="section-text">
+                    <div>
+                        <div>
+                            <p className="section-title">{language?.main_01_29}</p>
+                            <p className="section-text-01">{language?.main_01_30}</p>
+                            <div>
+                                <Link href="#" className="btn-01">{language?.main_01_31}</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-06">
+                <Image src="/images/main/section-06-bg.png" alt="" width={1920} height={998}/>
+                <div>
+                    <div>
+                        <div>
+                            <p>30+</p>
+                            <p>{language?.main_01_32}</p>
+                        </div>
+                        <div>
+                            <p>500+</p>
+                            <p>{language?.main_01_33}</p>
+                        </div>
+                        <div>
+                            <p>60</p>
+                            <p>{language?.main_01_34}</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-07 main-section">
+                <Image src="/images/main/section-04-bg.png" alt="" width={1922} height={1087}/>
+                <div className="section-text">
+                    <div>
+                        <div>
+                            <p className="section-title">{language?.main_01_35}</p>
+                            <p className="section-text-01">{language?.main_01_36}</p>
+                            <div>
+                                <Link href="#" className="btn-01">{language?.main_01_37}</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
