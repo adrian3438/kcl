0c6fdd9ffb040674996d9e456449d86505978ac9
jdlee <jdlee@bmcnet.kr>
2024-12-11 13:43:41 +0900

    카탈로그 링크 연결 완료

--- src/app/(pages)/media/catalog/page.tsx
+++ src/app/(pages)/media/catalog/page.tsx
@@ -17,85 +17,85 @@ export default async function Catalog({searchParams : {lang}} : any) {
                 </ul>
                 <ul className="catalog-list">
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/kcl-pq-2021-may-210515/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-01.jpg" alt="" width={289} height={217}/>
                             <p>{language?.catalog_01}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/kcl-company-product-guide-new/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-02.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_02}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/CONTROL(FINAL)/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-03.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_03}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/tunnion-dbb-ball-valve-_rev1/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-04.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_04}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/plugvalve/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-05.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_05}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/KCL%20DBB%20LUB%20Plug%20Catalouge%20New/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-06.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_06}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/butterflyall-141106/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-07.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_07}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/pigv-rew-160119/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-08.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_08}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/knife-gate_metalseatkgv/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-09.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_09}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/knife-gate_softseatkgv/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-10.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_10}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/CastingBallValve/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-11.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_11}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/CastingValve(GA%20GL%20CH)/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-12.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_12}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/ForgedBallValve/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-13.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_13}</p>
                         </Link>
                     </li>
                     <li>
-                        <Link href="#">
+                        <Link href="/catalog/DUAL%20CHECK-130716/index.html" target="_blank">
                             <Image src="/images/sub/catalog/catalog-list-14.jpg" alt="" width={250} height={333}/>
                             <p>{language?.catalog_14}</p>
                         </Link>
