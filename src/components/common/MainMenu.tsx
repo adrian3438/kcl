a8e9a64848a1b34b42ef0f8d8da43b75e53b9389
jdlee <jdlee@bmcnet.kr>
2025-02-18 17:27:27 +0900

    - 사용자 상단 메뉴 수정

--- src/components/common/MainMenu.tsx
+++ src/components/common/MainMenu.tsx
@@ -30,46 +30,48 @@ export default function MainMenu({language, headerLeave}: Props) {
                 <li onMouseOver={() => handleMouseover()}><Link href="/contact">{language?.header_5}</Link></li>
 
             </ul>
-            <ul className={`depth2 ${depth2Visible ? 'on' : ''}`}>
-                <li>
-                    {/* <ul>
-                        <li><Link href="/">{language?.header_0}</Link></li>
-                    </ul> */}
-                </li>
-                <li>
-                    <ul>
-                        <li><Link href="/about-us/management-solution">{language?.header_1_1}</Link></li>
-                        <li><Link href="/about-us/company-concept">{language?.header_1_2}</Link></li>
-                        <li><Link href="/about-us/greeting">{language?.header_1_3}</Link></li>
-                        <li><Link href="/about-us/history">{language?.header_1_4}</Link></li>
-                        <li><Link href="/about-us/partner">{language?.header_1_5}</Link></li>
-                        <li><Link href="/about-us/certificates">{language?.header_1_6}</Link></li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        <li><Link href="/product/forged-ball-valve">{language?.header_2_1}</Link></li>
-                        <li><Link href="/product/casted-ball-valve">{language?.header_2_2}</Link></li>
-                        <li><Link href="/product/plug-valve">{language?.header_2_3}</Link></li>
-                        <li><Link href="/product/casting-valve">{language?.header_2_4}</Link></li>
-                        <li><Link href="/product/butterfly-valve">{language?.header_2_5}</Link></li>
-                        <li><Link href="/product/control-valve">{language?.header_2_6}</Link></li>
-                        <li><Link href="/product/post-indicator-gate-valve">{language?.header_2_7}</Link></li>
-                        <li><Link href="/product/knife-gate-valve">{language?.header_2_9}</Link></li>
-                    </ul>
-                </li>
-                {/*<li>&nbsp;</li>*/}
-                <li>
-                    <ul>
-                        <li><Link href="/media/video">{language?.header_4_4}</Link></li>
-                        <li><Link href="/media/catalog">{language?.header_4_2}</Link></li>
-                        <li><Link href="/media/pq">{language?.header_4_5}</Link></li>
-                        <li><Link href="/media/3d-modeling">{language?.header_4_3}</Link></li>
-                        {/* <li><Link href="/media/blog">{language?.header_4_1}</Link></li> */}
-                    </ul>
-                </li>
-                <li>&nbsp;</li>
-            </ul>
+            <div className={`depth2 ${depth2Visible ? 'on' : ''}`}>
+                <ul>
+                    <li>
+                        {/* <ul>
+                            <li><Link href="/">{language?.header_0}</Link></li>
+                        </ul> */}
+                    </li>
+                    <li>
+                        <ul>
+                            <li><Link href="/about-us/management-solution">{language?.header_1_1}</Link></li>
+                            <li><Link href="/about-us/company-concept">{language?.header_1_2}</Link></li>
+                            <li><Link href="/about-us/greeting">{language?.header_1_3}</Link></li>
+                            <li><Link href="/about-us/history">{language?.header_1_4}</Link></li>
+                            <li><Link href="/about-us/partner">{language?.header_1_5}</Link></li>
+                            <li><Link href="/about-us/certificates">{language?.header_1_6}</Link></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li><Link href="/product/forged-ball-valve">{language?.header_2_1}</Link></li>
+                            <li><Link href="/product/casted-ball-valve">{language?.header_2_2}</Link></li>
+                            <li><Link href="/product/plug-valve">{language?.header_2_3}</Link></li>
+                            <li><Link href="/product/casting-valve">{language?.header_2_4}</Link></li>
+                            <li><Link href="/product/butterfly-valve">{language?.header_2_5}</Link></li>
+                            <li><Link href="/product/control-valve">{language?.header_2_6}</Link></li>
+                            <li><Link href="/product/post-indicator-gate-valve">{language?.header_2_7}</Link></li>
+                            <li><Link href="/product/knife-gate-valve">{language?.header_2_9}</Link></li>
+                        </ul>
+                    </li>
+                    <li>&nbsp;</li>
+                    <li>
+                        <ul>
+                            <li><Link href="/media/video">{language?.header_4_4}</Link></li>
+                            <li><Link href="/media/catalog">{language?.header_4_2}</Link></li>
+                            <li><Link href="/media/pq">{language?.header_4_5}</Link></li>
+                            <li><Link href="/media/3d-modeling">{language?.header_4_3}</Link></li>
+                            {/* <li><Link href="/media/blog">{language?.header_4_1}</Link></li> */}
+                        </ul>
+                    </li>
+                    <li>&nbsp;</li>
+                </ul>
+            </div>
         </nav>
     )
 }
